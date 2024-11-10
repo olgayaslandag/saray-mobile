1355e29c5212404c0b078f33b412302061cb7961
Olgay <olgayaslandag@gmail.com>
2024-11-10 17:38:09 +0300

    update

--- app/components/PdfViewer.js
+++ app/components/PdfViewer.js
@@ -1,19 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { Modal, View } from 'react-native';
 import { WebView } from 'react-native-webview';
-import Constants from 'expo-constants';
-import { StyleSheet } from 'react-native';
+import { Button, Text } from 'native-base';
 
-export default function PdfViewer({ uri }) {
+
+export default function PdfViewer({ uri }) {  
+
+  const [modalVisible, setModalVisible] = useState(false);
+  useEffect(() => {    
+    setModalVisible(uri ? true : false);    
+  }, [uri]);
+  const googleDocsUrl = `https://docs.google.com/viewer?url=${encodeURIComponent(uri)}`;
+
+
   return (
-    <WebView
-      style={styles.container}
-      source={{ uri }}
-    />
+    <View style={{ flex: 1 }}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+          <View style={{ flex: 1, marginTop: 0, backgroundColor: 'white' }}>
+            <View style={{ alignItems: 'flex-end' }}>
+              <Button size="sm" variant="ghost" onPress={() => setModalVisible(false)}>
+                <Text>Kapat</Text>                
+              </Button>
+            </View>
+            <WebView 
+              source={{ uri: encodeURI(uri) }}
+              style={{ flex: 1 }}
+              startInLoadingState
+            />
+          </View>
+        </View>
+      </Modal>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Constants.statusBarHeight,
-  },
-});
--- app/components/document/DocumentList.js
+++ app/components/document/DocumentList.js
@@ -1,35 +1,65 @@
-import { Box, Text, Image, Stack, HStack, VStack } from "native-base";
-import { Dimensions, Linking } from "react-native";
+import { useState, useEffect } from "react";
+import { Box, Text, Image, Stack, HStack, VStack, View } from "native-base";
+import { Dimensions, Linking, TouchableOpacity, ActivityIndicator } from "react-native";
+import PdfViewer from "../PdfViewer";
 
 
 const imageSize = Dimensions.get("window").width / 3 - 20;
 
 export default function DocumentList({ dirs, display }) {
+  const [selected, setSelected] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (dirs && dirs.length >= 0) {
+      setIsLoading(false);
+    }
+  }, [dirs]);
+
+
+  function HandleClick(path) {
+    setSelected("https://saray.com/drive/" + path);
+  }
+
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Yükleniyor...</Text>
+      </View>
+    );
+  }
+
+
 
   return (
-    <Stack space={4} mb={4} style={{display: display}}>
-      <VStack justifyContent="space-between">
-        {dirs.map((dir, dirIndex) => (
-          <Box key={dirIndex} mb={4}>
-            <Text mb={2} fontWeight="bold" fontSize="18">{dir.title.split(" | ")[1]}</Text>
+    <View>
+      <PdfViewer uri={selected} />
+      <Stack space={4} mb={4} style={{display: display}}>
+        <VStack justifyContent="space-between">
+          {dirs.map((dir, dirIndex) => (
+            <Box key={dirIndex} mb={4}>
+              <Text mb={2} fontWeight="bold" fontSize="18">{dir.title.split(" | ")[1]}</Text>
 
-            {Array.from({ length: Math.ceil(dir.files.length / 3) }).map((_, rowIndex) => (
-              <HStack key={rowIndex} space={3} justifyContent="space-between" mb={4}>
-                {dir.files.slice(rowIndex * 3, rowIndex * 3 + 3).map((item, fileIndex) => (
-                  <Box key={fileIndex} style={{ width: imageSize, alignItems: "center" }} onPress={async () => await Linking.openURL('https://google.com')}>
-                    <Image
-                      source={{ uri: item.thumbnail }}
-                      alt="thumbnail"
-                      style={{ width: imageSize, height: imageSize * 1.5, borderRadius: 10 }}
-                    />
-                    <Text style={{fontSize: 12}}>{item.title}</Text>
-                  </Box>
-                ))}
-              </HStack>
-            ))}
-          </Box>
-        ))}
-      </VStack>
-    </Stack>
+              {Array.from({ length: Math.ceil(dir.files.length / 3) }).map((_, rowIndex) => (
+                <HStack key={rowIndex} space={3} justifyContent="space-between" mb={4}>
+                  {dir.files.slice(rowIndex * 3, rowIndex * 3 + 3).map((item, fileIndex) => (
+                    <TouchableOpacity key={fileIndex} style={{ width: imageSize, alignItems: "center" }} onPress={() => HandleClick(item.path)}>
+                      <Image
+                        source={{ uri: item.thumbnail }}
+                        alt="thumbnail"
+                        style={{ width: imageSize, height: imageSize * 1.5, borderRadius: 10 }}
+                      />
+                      <Text style={{fontSize: 12}}>{item.title}</Text>
+                    </TouchableOpacity>
+                  ))}
+                </HStack>
+              ))}
+            </Box>
+          ))}
+        </VStack>
+      </Stack>
+    </View>
   );
 }
